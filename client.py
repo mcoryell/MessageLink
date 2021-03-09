3da22912bf615c2be6e351346675522c68fb4d05
Maverick Coryell <maverick.coryell@gmail.com>
2021-03-09 18:01:44 -0500

    Initial project setup and configuration

---
+++ client.py
@@ -0,0 +1,26 @@
+# Client code will go here
+
+def main():
+    """
+    Summary line.
+
+    Extended description of function.
+
+    Parameters
+    ----------
+    arg1 : int
+        Description of arg1
+    arg2 : str
+        Description of arg2
+
+    Returns
+    -------
+    int
+        Description of return value
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
