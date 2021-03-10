4aecfafd76c0884aba171e3a95afb6ec7ae6da7c
Maverick Coryell <maverick.coryell@gmail.com>
2021-03-10 11:51:35 -0500

    Made a simple TCP connection to the server from the client program.

--- client.py
+++ client.py
@@ -1,25 +1,32 @@
-# Client code will go here
+import socket
+
+HOST = '35.185.50.240'  # The server's hostname or IP address
+PORT = 30330            # The port used by the server
 
 def main():
     """
-    Summary line.
+    Sends a 'Hello World!' message to our server using a TCP socket.
 
-    Extended description of function.
+    Creates a TCP socket, connects to the server, sends a message, receives the echo result,
+    and then prints the response.
 
     Parameters
     ----------
-    arg1 : int
-        Description of arg1
-    arg2 : str
-        Description of arg2
+    void : void type
+        No parameters used
 
     Returns
     -------
-    int
-        Description of return value
+    void
+        Nothing of values is returned
 
     """
-    pass
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'Hello World!') # Send 'Hello World!' to server
+        data = s.recv(1024)
+
+    print('Received', repr(data))
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,25 +1,38 @@
-# Server code will go here
+import socket
+
+HOST = '0.0.0.0'    # Listen from any IP
+PORT = 30330        # Port to listen on (non-privileged ports are > 1023)
 
 def main():
     """
-    Summary line.
+    Echos a 'Hello World!' message back to the client using a TCP socket
 
-    Extended description of function.
+    Creates a TCP socket, binds it to a specific port, listens for client connections, prints the IP of
+    the connected client, and then echos the received message back to the client.
 
     Parameters
     ----------
-    arg1 : int
-        Description of arg1
-    arg2 : str
-        Description of arg2
+    void : void type
+        No parameters used
 
     Returns
     -------
-    int
-        Description of return value
+    void
+        Nothing of value is returned
 
     """
-    pass
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print('Received: ', repr(data))
+                conn.sendall(data)  # Send 'Hello World!' back
 
 
 if __name__ == '__main__':
