ca1be51e11adac7f83012405536cd7dab87c8d5a
Maverick Coryell <maverick.coryell@gmail.com>
2021-03-30 13:02:41 -0400

    v2 of our client-server architecture is mostly done, we just need to encrypt the AES info on the client-side using the server's public key and subsequently decrypt the aes_info on the server-side using the server's private key. The messages themselves in v2 files are encrypted using the AES_info. Need to find a certificate authority that the client's can request our server's public key from.

---
+++ client_v2.py
@@ -0,0 +1,92 @@
+import os
+import socket
+import sys
+import json
+
+from base64 import b64encode
+from cryptography.hazmat.primitives import serialization, padding, hashes
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding as p
+
+HOST = '35.185.50.240'  # The server's hostname or IP address
+PORT = 30330            # The port used by the server
+
+
+def create_aes_info():
+    aes_info = {
+        'key': os.urandom(32),
+        'iv': os.urandom(16)
+    }
+
+    return aes_info
+
+
+def encrypt_message(message, aes_info):
+    cipher = Cipher(algorithms.AES(aes_info['key']), modes.CBC(aes_info['iv']))
+    encryptor = cipher.encryptor()
+
+    # Pad the message to be a multiple of 128 bits for the AES algorithm
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(message.encode()) + padder.finalize()
+
+    # Encrypt the data
+    cipher_text = encryptor.update(padded_data) + encryptor.finalize()
+    return cipher_text
+
+
+def decrypt_message(encrypted_server_response, aes_info):
+    cipher = Cipher(algorithms.AES(aes_info['key']), modes.CBC(aes_info['iv']))
+    decrypter = cipher.decryptor()
+
+    # Decrypt the data
+    padded_server_response = decrypter.update(encrypted_server_response) + decrypter.finalize()
+
+    # Unpad the server response message
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_server_response = unpadder.update(padded_server_response) + unpadder.finalize()
+    return decrypted_server_response
+
+
+def main():
+    # Initialize client program with welcome prompt
+    print('Welcome to MessageLink!')
+    print('Status: A new MessageLink Client connection is being created.\n')
+
+    # Create the AES key and iv
+    aes_info = create_aes_info()
+    prepped_aes_info = {
+        'key': b64encode(aes_info['key']).decode('utf-8'),
+        'iv': b64encode(aes_info['iv']).decode('utf-8')
+    }
+
+    print('AES-256 Key (Base64 Encoded):\n', prepped_aes_info['key'], '\n', sep='')
+    print('AES-256 Initial Value (Base64 Encoded):\n', prepped_aes_info['iv'], '\n', sep='')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Allow the client to connect
+        s.connect((HOST, PORT))
+        print('Status: Connection has been established.\n')
+
+        # Share AES info with the server
+        s.send(json.dumps(prepped_aes_info).encode('utf-8'))
+
+        while True:
+            message = input("Message to Send: ")
+            s.send(encrypt_message(message, aes_info))
+            encrypted_server_response = s.recv(1024)
+            if message == 'MessageLink -terminate':
+                break
+            decrypted_server_response = decrypt_message(encrypted_server_response, aes_info)
+            print('Ciphertext received from server: ', encrypted_server_response, sep='')
+            print(decrypted_server_response.decode('utf-8'))
+
+        s.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        print('Additional command line arguments are unnecessary to run this program.')
+        exit()
+
+    main()
---
+++ server_v2.py
@@ -0,0 +1,110 @@
+import socket
+import sys
+import json
+from _thread import *
+
+from base64 import b64decode
+from cryptography.hazmat.primitives import serialization, padding, hashes
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding as p
+
+HOST = '0.0.0.0'  # Listen from any IP
+PORT = 30330  # Port to listen on (non-privileged ports are > 1023)
+client_list = []  # List to hold all the connections of clients
+
+
+def encrypt_message(server_response, aes_info):
+    cipher = Cipher(algorithms.AES(aes_info['key']), modes.CBC(aes_info['iv']))
+    encryptor = cipher.encryptor()
+
+    # Pad the server response to be a multiple of 128 bits for the AES algorithm
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(server_response.encode()) + padder.finalize()
+
+    # Encrypt the data
+    cipher_text = encryptor.update(padded_data) + encryptor.finalize()
+    return cipher_text
+
+
+def decrypt_message(encrypted_message, aes_info):
+    cipher = Cipher(algorithms.AES(aes_info['key']), modes.CBC(aes_info['iv']))
+    decrypter = cipher.decryptor()
+
+    # Decrypt the data
+    padded_message = decrypter.update(encrypted_message) + decrypter.finalize()
+
+    # Unpad the server response message
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_message = unpadder.update(padded_message) + unpadder.finalize()
+    return decrypted_message
+
+
+def client_thread(client):
+    while True:
+        data = client['connection'].recv(1024)
+        decrypted_data = decrypt_message(data, client['aes_info'])
+        if decrypted_data.decode('utf-8') == 'MessageLink -terminate':
+            break
+        print('Ciphertext received from ', client['ip_address'], ': ', data, sep='')
+        print('Message received from ', client['ip_address'], ': ', decrypted_data.decode('utf-8'), sep='')
+        server_response = 'Server Response: Message has been received as \'' + decrypted_data.decode('utf-8') + '\''
+        client['connection'].send(encrypt_message(server_response, client['aes_info']))
+
+    print('Status: A MessageLink Client with IP Address, ', client['ip_address'], ', has disconnected.', sep='')
+    client['connection'].close()
+    client_list.remove(client)
+
+
+def main():
+    # Initialize server program with welcome prompt
+    print('Welcome to MessageLink!')
+    print('Status: A new MessageLink Server is being created.')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((HOST, PORT))
+        except OSError:
+            print(OSError)
+
+        print('Status: Currently awaiting a connection.')
+        s.listen(100)  # Listens for up to 100 client connections
+
+        while True:
+            # Allow the client to connect
+            conn, addr = s.accept()
+            print('Status: A new MessageLink Client connection has been established. Client:', addr[0], '\n')
+
+            # Retrieve AES key and iv from client
+            prepped_aes_info = json.loads(conn.recv(1024).decode('utf-8'))
+
+            print('AES-256 Key (Base64 Encoded) used in Connection with ', addr[0], ':\n',
+                  prepped_aes_info['key'], '\n', sep='')
+            print('AES-256 Initial Value (Base64 Encoded) used in Connection with ', addr[0], ':\n',
+                  prepped_aes_info['iv'], '\n', sep='')
+
+            # Add the new client connection to the list.
+            # Note: The client's aes info is in base64 encoded strings and needed to be converted to bytes
+
+            aes_info = {
+                'key': b64decode(prepped_aes_info['key']),
+                'iv': b64decode(prepped_aes_info['iv'])
+            }
+
+            client = {
+                "connection": conn,
+                "ip_address": addr[0],
+                "aes_info": aes_info,
+            }
+            client_list.append(client)
+
+            # Start messaging
+            start_new_thread(client_thread, (client,))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        print('Additional command line arguments are unnecessary to run this program.')
+        exit()
+
+    main()
